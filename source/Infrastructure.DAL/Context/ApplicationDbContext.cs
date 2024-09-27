9ca1769dfee3fec6abc14fa37e23767964d5769d
hauleguitare <alexander.dutyone@gmail.com>
2024-09-27 16:16:55 +0700

    [update]: update application db context

--- source/Infrastructure.DAL/Context/ApplicationDbContext.cs
+++ source/Infrastructure.DAL/Context/ApplicationDbContext.cs
@@ -1,13 +1,15 @@
 ﻿
+using Infrastructure.DAL.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.DAL.Context;
 
-public class ApplicationDbContext: IdentityDbContext<IdentityUser>
+public class ApplicationDbContext: IdentityDbContext<ApplicationUser, ApplicationRole, string>
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :
         base(options)
     { }
+    
 }
