f52c4bf1d039865c30a95b54065956ad3d72f793
hauleguitare <alexander.dutyone@gmail.com>
2024-10-10 18:19:23 +0700

    fix add cqrs result factory

--- source/MonoModularNet.Infrastructure.CQRS/Response/CqrsResult.cs
+++ source/MonoModularNet.Infrastructure.CQRS/Response/CqrsResult.cs
@@ -14,6 +14,22 @@ public class CqrsResult: ICqrsResult
     public string[]? Errors { get; set; }
     public string[]? Messages { get; set; }
     public bool IsSuccess { get; set; } = false;
+
+    public static CqrsResult Success()
+    {
+        return new CqrsResult()
+        {
+            IsSuccess = true
+        };
+    }
+    
+    public static CqrsResult Failure()
+    {
+        return new CqrsResult()
+        {
+            IsSuccess = false,
+        };
+    }
 }
 
 
@@ -23,4 +39,21 @@ public class CqrsResult<TResponse>: ICqrsResult
     public string[]? Errors { get; set; }
     public string[]? Messages { get; set; }
     public bool IsSuccess { get; set; } = false;
+    
+    public static CqrsResult<TResponse> Success(TResponse response)
+    {
+        return new CqrsResult<TResponse>()
+        {
+            IsSuccess = true,
+            Result = response
+        };
+    }
+    
+    public static CqrsResult Failure()
+    {
+        return new CqrsResult()
+        {
+            IsSuccess = false,
+        };
+    }
 }
